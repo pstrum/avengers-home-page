1f84f3c5ddc116e85880d1af6eae538d4ca7df95
Peter Strumolo <Peter Strumolo>
2015-09-15 12:43:15 -0700

    updated the section header styles in scss file

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,3 +1,6 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
 gulp.task("sass", function() {
   gulp.src("sass/**/*.scss")
     .pipe(sass().on("error", sass.logError))
@@ -7,4 +10,3 @@ gulp.task("sass", function() {
 gulp.task("default", ["sass"], function() {
   gulp.watch("sass/**/*.scss", ["sass"]);
 });
-
